fe04a16e6b8aa83eb27e77d60b57f798581490c7
Andrew Griffith <griffithac@gamil.com>
2013-12-10 13:53:03 -0800

    namespace

--- lib/griffith_common/engine.rb
+++ lib/griffith_common/engine.rb
@@ -1,7 +1,8 @@
 module GriffithCommon
   class Engine < ::Rails::Engine
-    initializer "griffith_common.include_helpers" do |app|
-      ActionView::Base.send :include, GriffithCommon::ApplicationHelper
-    end
+    # initializer "griffith_common.include_helpers" do |app|
+    #   ActionView::Base.send :include, GriffithCommon::ApplicationHelper
+    # end
+    isolate_namespace GriffithCommon
   end
 end
