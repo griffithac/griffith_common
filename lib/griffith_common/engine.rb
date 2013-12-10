4cfb91edbe922fb8583aea8d982c8d5a165dfcfd
Andrew Griffith <griffithac@gamil.com>
2013-12-10 14:14:47 -0800

    fix

--- lib/griffith_common/engine.rb
+++ lib/griffith_common/engine.rb
@@ -3,6 +3,6 @@ module GriffithCommon
     # initializer "griffith_common.include_helpers" do |app|
     #   ActionView::Base.send :include, GriffithCommon::ApplicationHelper
     # end
-    isolate_namespace GriffithCommon
+    # isolate_namespace GriffithCommon
   end
 end
