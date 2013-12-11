458bda4274059e8d076f6b8c24c6f924a66cd8c4
Andrew Griffith <griffithac@gamil.com>
2013-12-10 16:16:10 -0800

    initialize

--- lib/griffith_common/engine.rb
+++ lib/griffith_common/engine.rb
@@ -2,11 +2,12 @@ require 'griffith_common/application_helper.rb'
 require 'griffith_common/application_controller.rb'
 
 module GriffithCommon
-  class Engine < ::Rails::Engine
-
-    ActionView::Base.send :include, GriffithCommon::ApplicationHelper
-    ActionController::Base.send :include, GriffithCommon::ApplicationController
-  
+  class Engine < ::Rails::Engine 
+    
+    initializer "griffith_common.loader" do |app|
+      ActionView::Base.send :include, GriffithCommon::ApplicationHelper
+      ActionController::Base.send :include, GriffithCommon::ApplicationController
+    end
 
   end
 end
