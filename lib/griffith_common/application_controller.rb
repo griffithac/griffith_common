92b87f0a91e45972ec6d18823a748c0b55f7c4d7
Andrew Griffith <griffithac@gmail.com>
2014-05-12 00:03:27 -0700

    whitespace

--- lib/griffith_common/application_controller.rb
+++ lib/griffith_common/application_controller.rb
@@ -1,6 +1,6 @@
 module GriffithCommon
   module ApplicationController #< ActionController::Base
-  
+
     DEFAULT_ROWS = 20
     MIN_ROWS     = 1
     MAX_ROWS     = 100
@@ -16,7 +16,6 @@ module GriffithCommon
       end
     end
 
-
     def sort_direction direction = DEFAULT_SORT
       if %w[asc desc].include?(params[:direction])
         params[:direction]
@@ -25,7 +24,6 @@ module GriffithCommon
       end
     end
 
-
     def sort_column
       if params[:sort].present? && sortable_columns.include?(params[:sort])
         params[:sort]
@@ -33,15 +31,13 @@ module GriffithCommon
         default_sort_column
       end
     end
-    
 
     def default_sort_column
       sortable_columns.compact.first
     end
 
-
     def sortable_columns
-      eval("#{params[:controller].classify}.search_columns").values 
+      eval("#{params[:controller].classify}.search_columns").values
     end
 
   end
