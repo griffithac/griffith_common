dab3e1172ad200ea939de4d5037123c02fbbec74
Andrew Griffith <griffithac@gmail.com>
2014-10-19 00:27:10 -0700

    add option and refactor
    add option to customize title. rename heading to resource. and do some
    style fixes.

--- lib/griffith_common/bootstrap_helpers.rb
+++ lib/griffith_common/bootstrap_helpers.rb
@@ -1,15 +1,16 @@
 module GriffithCommon
   module BootstrapHelpers
 
-    def panel_for heading, opts = {}
+    def panel_for(resource, opts = {})
       opts = {id: "#{heading.to_s.dasherize}", class: 'panel-default'}.merge(opts)
-      heading = heading.to_s.titleize
+      title = opts.fetch(:title) { resource.to_s.titleize }
 
       content_tag :div, id: opts[:id],class: "panel #{opts[:class]}" do
         concat content_tag(:div,
-                           content_tag(:h3,
-                                       heading,
-                                       class: 'panel-title center'),
+                           content_tag(
+                             :h3,
+                             title,
+                             class: 'panel-title center'),
                            class: 'panel-heading')
         yield
       end.html_safe
