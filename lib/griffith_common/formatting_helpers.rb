1358bac9bcfb690982b6294646f11dee90b4a866
Andrew Griffith <griffithac@gmail.com>
2014-11-17 18:00:41 +0200

    add helpers extracted from griffithind_co

--- lib/griffith_common/formatting_helpers.rb
+++ lib/griffith_common/formatting_helpers.rb
@@ -1,11 +1,23 @@
 module GriffithCommon
   module FormattingHelpers
-
-    def currency number
-      number_to_currency(number, :unit => "",
-                                 :separator => ".",
-                                 :delimiter => ",")
+    def simple_number(number)
+      number == number.to_i ? number.to_i : number.round(4)
     end
 
+    def decimal_to_percentage(value, precision)
+      value = 0.00 if value.blank?
+      number_to_percentage(value * 100, precision: precision)
+    end
+
+    def currency(number, unit: "", separator: ".", delimiter: ",", precision: 2)
+      number_to_currency(number, unit: unit,
+                                 separator: separator,
+                                 delimiter: delimiter,
+                                 precision: precision)
+    end
+
+    def quantity(number)
+      number_with_precision(number.to_f, precision: 2, delimiter: ',')
+    end
   end
 end
