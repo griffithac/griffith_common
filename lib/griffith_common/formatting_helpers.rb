3dff9b8dd845fa6e8717788d9e13f452a10b6530
Andrew Griffith <griffithac@gmail.com>
2014-12-29 23:18:59 -0700

    rename and alias #precentage
    add a default precision

--- lib/griffith_common/formatting_helpers.rb
+++ lib/griffith_common/formatting_helpers.rb
@@ -4,10 +4,11 @@ module GriffithCommon
       number == number.to_i ? number.to_i : number.round(4)
     end
 
-    def decimal_to_percentage(value, precision)
+    def percentage(value, precision = 2)
       value = 0.00 if value.blank?
       number_to_percentage(value * 100, precision: precision)
     end
+    alias_method :decimal_to_percentage, :percentage
 
     def currency(number, unit: "", separator: ".", delimiter: ",", precision: 2)
       number_to_currency(number, unit: unit,
