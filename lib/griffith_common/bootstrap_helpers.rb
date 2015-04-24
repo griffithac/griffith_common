21aa2e79b8c6678bdcd96c9392816de4db68b1cf
Andrew Griffith <griffithac@gmail.com>
2015-04-23 23:10:23 -0700

    extract from griffithind_co

--- lib/griffith_common/bootstrap_helpers.rb
+++ lib/griffith_common/bootstrap_helpers.rb
@@ -24,6 +24,59 @@ module GriffithCommon
       "<span class='label #{opts[:class]}'>#{name}</span>".html_safe
     end
 
+    def modal_for(heading,
+                  id: nil,
+                  button_label: "#{ heading.to_s.titleize }",
+                  submit: "create #{ heading }",
+                  button_class: 'btn-xs btn-primary',
+                  button_style: '',
+                  modal_class: '')
+
+      id ||= heading.to_s.downcase.underscore
+      title = heading.to_s.titleize
+      x = '&times;'.html_safe
+      concat button_tag(button_label, id: "#{ id }-button",
+                                      href: "##{ id }",
+                                      data: { toggle: 'modal' },
+                                      class: "btn #{ button_class }",
+                                      style: "#{ button_style }")
+      content_tag :div, class: 'modal fade',
+                        id: "#{ id }",
+                        role: 'dialog',
+                        tabindex: '-1',
+                        'z-index' => '-1',
+                        'aria-labelledby' => "#{ heading }",
+                        'aria-hidden' => 'true' do
+        content_tag :div, class: "modal-dialog #{ modal_class }" do
+          content_tag :div, class: 'modal-content' do
+            content = content_tag(:div, class: 'modal-header') do
+              header = button_tag(x, class: 'close',
+                                     type: 'button',
+                                     data: { dismiss: 'modal' },
+                                     'aria-hidden' => true)
+              header << content_tag(:h4, class: 'modal-title',
+                                         id: "#{ id }") do
+                "#{ title }".html_safe
+              end
+            end
+            content << content_tag(:div, class: 'modal-body') do
+              yield if block_given?
+            end
+          end
+        end
+      end
+    end
+
+    def modal_action_bar
+      content_tag :div, class: 'modal-action-bar' do
+        button = content_tag(:span) do
+          yield if block_given?
+        end
+        button << button_tag('Close', class: 'btn btn-default',
+                                      data: { dismiss: 'modal' })
+      end
+    end
+
     module Icon
       def icon(name, classes = '')
         "<i class='fa fa-#{name.to_s} fa-fw #{classes}'></i>".html_safe
