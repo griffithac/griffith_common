d343cbc3685e55e5d5ab12feb3d6fc72ae4ebffe
Andrew Griffith <griffithac@gmail.com>
2015-05-11 21:46:26 -0700

    simplify formaters

--- lib/griffith_common/formatting_helpers.rb
+++ lib/griffith_common/formatting_helpers.rb
@@ -1,31 +1,26 @@
 module GriffithCommon
   module FormattingHelpers
-    def simple_number(number)
-      if number.nil? || number.zero?
-        '-'
-      else
-        number == number.to_i ? number.to_i : number.round(4)
-      end
+    def simple_number(val)
+      return '-' if val.nil? || val.zero?
+      val == val.to_i ? val.to_i : val.round(4)
     end
 
-    def percentage(value, precision = 2)
-      if value.nil? || value.zero?
-        '-'
-      else
-        value = 0.00 if value.blank?
-        number_to_percentage(value * 100, precision: precision)
-      end
+    def percentage(val, precision = 2)
+      return '-' if val.nil? || val.zero?
+      number_to_percentage(val * 100, precision: precision)
     end
     alias_method :decimal_to_percentage, :percentage
 
-    def currency(number, unit: "", separator: ".", delimiter: ",", precision: 2)
-      number_to_currency(number, unit: unit,
-                                 separator: separator,
-                                 delimiter: delimiter,
-                                 precision: precision)
+    def currency(val, unit: "", separator: ".", delimiter: ",", precision: 2)
+      return '-' if val.nil? || val.zero?
+      number_to_currency(val, unit: unit,
+                              separator: separator,
+                              delimiter: delimiter,
+                              precision: precision)
     end
 
-    def quantity(number)
+    def quantity(val)
+      return '-' if val.nil? || val.zero?
       number_with_precision(number.to_f, precision: 2, delimiter: ',')
     end
   end
