16acc82685c5d09c91110b06eeae4c35fc0d5ee1
Andrew Griffith <griffithac@gmail.com>
2015-05-14 10:17:20 -0700

    ensure that val is a number

--- lib/griffith_common/formatting_helpers.rb
+++ lib/griffith_common/formatting_helpers.rb
@@ -1,18 +1,18 @@
 module GriffithCommon
   module FormattingHelpers
     def simple_number(val)
-      return '-' if val.nil? || val.zero?
+      return '-' if val.nil? || val.to_f.zero?
       val == val.to_i ? val.to_i : val.round(4)
     end
 
     def percentage(val, precision = 2)
-      return '-' if val.nil? || val.zero?
+      return '-' if val.nil? || val.to_f.zero?
       number_to_percentage(val * 100, precision: precision)
     end
     alias_method :decimal_to_percentage, :percentage
 
     def currency(val, unit: "", separator: ".", delimiter: ",", precision: 2)
-      return '-' if val.nil? || val.zero?
+      return '-' if val.nil? || val.to_f.zero?
       number_to_currency(val, unit: unit,
                               separator: separator,
                               delimiter: delimiter,
