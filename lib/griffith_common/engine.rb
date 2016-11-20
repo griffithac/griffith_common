57d627e130fdbbead1f3e35b94b200e78d593850
Andrew Griffith <griffithac@gmail.com>
2016-11-19 19:15:41 -0800

    remove code that has been extracted

--- lib/griffith_common/engine.rb
+++ lib/griffith_common/engine.rb
@@ -5,10 +5,6 @@ module GriffithCommon
     config.autoload_paths += Dir["#{config.root}/app/lib/**/*.rb"]
     config.autoload_paths += Dir["#{config.root}/lib/griffith_common/**"]
 
-    ActiveSupport.on_load :action_view do
-      include GriffithCommon::ApplicationHelper
-    end
-
     ActiveSupport.on_load :action_controller do
       include GriffithCommon::ApplicationController
     end
